1c8ba3cbca4d5c459e977c59ce45924938551f3b
Espen A. Kristiansen <post@espenak.net>
2011-02-17 00:45:20 +0100

    Some changes

--- src/Session.cc
+++ src/Session.cc
@@ -1,8 +1,11 @@
 #include "Session.h"
+#include <iostream>
 
 namespace socketevent {
     void stuff()
     {
-
+        if (true) {
+            std::cout << "hei" << std::endl;
+        }
     }
 } // namespace socketevent
--- test/Example.cc
+++ test/Example.cc
@@ -1,5 +1,10 @@
+#include "Session.h"
+#include <iostream>
+
+using namespace socketevent;
+
 int main(int argc, const char *argv[])
 {
-    
+    stuff();
     return 0;
 }
