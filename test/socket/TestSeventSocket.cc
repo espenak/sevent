f814329d33bc4b51857915ea9449567d5b4b6d92
Espen A. Kristiansen <post@espenak.net>
2011-02-19 00:35:12 +0100

    Basic test setup

---
+++ test/socket/TestSeventSocket.cc
@@ -0,0 +1,9 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TestSeventSocket
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE( TestSeventMain )
+{
+    BOOST_REQUIRE_EQUAL(1, 2);
+}
