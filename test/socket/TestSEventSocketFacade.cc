34ecbcf0090798269330baadb4c40a7aaf8ca445
Espen A. Kristiansen <post@espenak.net>
2011-02-20 03:06:03 +0100

    Sanity test for socket

--- test/socket/TestSEventSocketFacade.cc
+++ test/socket/TestSEventSocketFacade.cc
@@ -1,9 +1,34 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE TestSevent
+#define BOOST_TEST_MODULE TestSEventSocketFacade
 #include <boost/test/unit_test.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+#include "sevent/socket/Facade.h"
+#include "helpers.h"
+#include <iostream>
+
+using namespace sevent::socket;
 
 
-BOOST_AUTO_TEST_CASE( TestSEventMain )
+
+BOOST_AUTO_TEST_CASE( TestSEventSocketFacadeSanity )
 {
-    BOOST_REQUIRE_EQUAL(1, 2);
+    Facade_ptr facade = Facade::make();
+    AllEventsHandler allEventsHandler(2);
+    facade->setWorkerThreads(1,
+            boost::bind(boost::ref(allEventsHandler), _1, _2, _3));
+    Listener_ptr listener = facade->listen(Address::make("127.0.0.1", "9091"));
+    Session_ptr session = facade->connect(Address::make("127.0.0.1", "9091"));
+
+    char hello[6] = { 'h', 'e', 'l', 'l', 'o', '\0' };
+    session->sendEvent(EventData(10, hello, 6));
+    char die[6] = { 'w', 'o', 'r', 'l', 'd', '\0' };
+    session->sendEvent(EventData(20, die, 6));
+
+    facade->joinAllWorkerThreads();
+    
+    BOOST_REQUIRE_EQUAL(allEventsHandler.counter(), 2);
 }
---
+++ test/socket/helpers.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include "sevent/socket/Facade.h"
+
+class AllEventsHandler
+{
+    public:
+        typedef void result_type;
+    public:
+        AllEventsHandler(int expectedCalls) :
+            _counter(0), _expectedCalls(expectedCalls)
+        {}
+
+        void operator()(sevent::socket::Facade_ptr facade,
+                sevent::socket::Session_ptr session, sevent::socket::EventData eventData)
+        {
+            boost::lock_guard<boost::mutex> lock(counter_lock);
+            _counter ++;
+            if (_counter == _expectedCalls)
+            {
+                facade->service()->stop();
+            }
+        }
+
+        int counter() const
+        {
+            return _counter;
+        }
+
+    private:
+        int _counter;
+        int _expectedCalls;
+        boost::mutex counter_lock;
+};
