f6adafe009f060c7673fbd0965839bfee5e448d6
Espen A. Kristiansen <post@espenak.net>
2011-04-05 19:35:20 +0200

    More work

--- test/socket/TestEvent.cc
+++ test/socket/TestEvent.cc
@@ -12,19 +12,40 @@
 #include <iostream>
 
 
-struct Serialized
+struct ConstCharArray
 {
     const char* data;
     unsigned size;
-    Serialized(const char* data_, unsigned size_) : data(data_), size(size_) {}
+    ConstCharArray(const char* data_, unsigned size_) : data(data_), size(size_) {}
 };
-typedef boost::shared_ptr<Serialized> Serialized_ptr;
+typedef boost::shared_ptr<ConstCharArray> ConstCharArray_ptr;
 
-typedef boost::function<Serialized_ptr (boost::any& data)> serialize_t;
-typedef boost::function<boost::any (Serialized_ptr serialized)> deserialize_t;
+struct MutableCharArray
+{
+    char* data;
+    unsigned size;
+    MutableCharArray(char* data_, unsigned size_) : data(data_), size(size_) {}
+};
+typedef boost::shared_ptr<MutableCharArray> MutableCharArray_ptr;
 
-typedef std::pair<boost::any, serialize_t> AnyType;
-typedef std::vector<AnyType> AnyTypeVector;
+typedef boost::function<ConstCharArray_ptr (boost::any& data)> serialize_t;
+typedef boost::function<boost::any (MutableCharArray_ptr serialized)> deserialize_t;
+
+
+struct Buffer
+{
+    boost::any data;
+    serialize_t serializeFunc;
+    Buffer(boost::any& data_, serialize_t serializeFunc_) :
+        data(data_), serializeFunc(serializeFunc_) {}
+};
+typedef boost::shared_ptr<Buffer> Buffer_ptr;
+
+
+
+
+//typedef std::pair<boost::any, serialize_t> Buffer;
+typedef std::vector<Buffer_ptr> BufferVector;
 
 
 template<typename T>
@@ -43,20 +64,20 @@ typedef Array<int> IntArray;
 typedef boost::shared_ptr<IntArray> IntArray_ptr;
 
 
-const Serialized_ptr serializeInt(boost::any& data)
+ConstCharArray_ptr serializeInt(boost::any& data)
 {
     IntArray_ptr a = boost::any_cast<IntArray_ptr>(data);
-    return boost::make_shared<Serialized>(reinterpret_cast<const char*>(a->data.get()),
+    return boost::make_shared<ConstCharArray>(reinterpret_cast<const char*>(a->data.get()),
                                           a->size*sizeof(int));
 }
 
-AnyType deserializeInt(char* serialized, unsigned size)
+boost::any deserializeInt(char* serialized, unsigned size)
 {
     int* data = reinterpret_cast<int*>(serialized);
     assert((size%sizeof(int)) == 0);
     IntArray_ptr arr = boost::make_shared<IntArray>(boost::shared_array<int>(data),
                                                     size/sizeof(int));
-    return AnyType(arr, serializeInt);
+    return arr;
 }
 
 
@@ -65,21 +86,24 @@ BOOST_AUTO_TEST_CASE(VectorInOut)
     boost::shared_array<int> intarr = boost::shared_array<int>(new int[2]);
     intarr[0] = 10;
     intarr[1] = 20;
-    AnyType a = std::make_pair(boost::make_shared<IntArray>(intarr, 2),
-                               serializeInt);
-    Serialized_ptr serialized = a.second(a.first);
+    boost::any data = boost::make_shared<IntArray>(intarr, 2);
+    Buffer_ptr a = boost::shared_ptr<Buffer>(new Buffer(data,
+                                                        serializeInt));
+    ConstCharArray_ptr serialized = a->serializeFunc(a->data);
 
     // Simulate network transfer (copy the serialized data)
-    char* receivedData = new char[serialized->size]; 
+    char* receivedData = new char[serialized->size];
     std::memcpy(receivedData, serialized->data, serialized->size);
-    AnyType deserialized = deserializeInt(receivedData, serialized->size);
-    IntArray_ptr aOut = boost::any_cast<IntArray_ptr>(deserialized.first);
+
+    // Deserialize the "received data"
+    boost::any deserialized = deserializeInt(receivedData, serialized->size);
+    IntArray_ptr aOut = boost::any_cast<IntArray_ptr>(deserialized);
     std::cerr 
         << aOut->data[0] << ":"
         << aOut->data[1] << ":"
         << aOut->size << std::endl;
 
-    //AnyTypeVector vec;
+    //BufferVector vec;
     //std::string s = "Hello world";
     //int i = 10;
     //BOOST_REQUIRE_EQUAL();
